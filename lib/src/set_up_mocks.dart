f0afb0b3b2adb84d1b34c819cff7d5e424f6cb11
sitatec <sita.berete.3@gmail.com>
2021-01-17 16:33:17 -0500

    add requirement to publish package

--- lib/src/set_up_mocks.dart
+++ lib/src/set_up_mocks.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+/// Set up firebase core for tests.
 void setupFirebaseMocks() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
