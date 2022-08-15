832ef55e75faf98c07683db915066295badebf25
Michal <mmarciniak299@gmail.com>
2022-08-15 16:19:19 +0200

    chore: add more documentation on desired id generation behavior

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -3,8 +3,16 @@ import 'dart:math';
 const _pushChars =
     '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
 
+/// Requirements:
+/// - generated id should contain 20 characters
+/// - should prevent collisions if called more than once in a millisecond
+/// - should sort lexicographically (ids created earlier will appear before
+///   keys created later if sorted)
 final nextPushId = (() {
+  // prevents collisions if you push more than once in one millisecond
   var _lastPushTime = 0;
+  // if called more than once in one millisecond previous random chars
+  // will be used except incremented by 1
   final _lastRandChars = List<int>.generate(12, (index) => 0);
   final _random = Random.secure();
 
@@ -23,6 +31,7 @@ final nextPushId = (() {
     }
     assert(now == 0);
 
+    // add 8 timestamp chars
     var id = timestampChars.join();
 
     if (!duplicateTime) {
@@ -30,6 +39,8 @@ final nextPushId = (() {
         _lastRandChars[i] = _random.nextInt(64);
       }
     } else {
+      // timestamp hasn't changed since last push, thus will use
+      // previous random number incremented by 1
       late int i;
       for (i = 11; i >= 0 && _lastRandChars[i] == 63; i--) {
         _lastRandChars[i] = 0;
@@ -37,6 +48,7 @@ final nextPushId = (() {
       _lastRandChars[i]++;
     }
 
+    // add 12 random chars
     for (var i = 0; i < 12; i++) {
       id += _pushChars[_lastRandChars[i]];
     }
